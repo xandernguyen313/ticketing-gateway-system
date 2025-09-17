980fa2790d19308f95d977a25189b46cbe22a108
Xander Nguyen <xander.nguyen313@gmail.com>
2025-09-17 11:24:32 -0500

    first commit

---
+++ ticketing-gateway/src/main/java/com/example/ticketing_gateway/TicketingGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketing_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingGatewayApplication.class, args);
+	}
+
+}
---
+++ ticketing-gateway/src/test/java/com/example/ticketing_gateway/TicketingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketing_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
